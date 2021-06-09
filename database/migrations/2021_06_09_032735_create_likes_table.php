5f5c49c1ff8d95e09b3b3d8fc64790a57e7d1c13
RedGradient <>
2021-06-09 16:26:11 +1100

    Fix: added user_id, post_id

--- database/migrations/2021_06_09_032735_create_likes_table.php
+++ database/migrations/2021_06_09_032735_create_likes_table.php
@@ -16,7 +16,8 @@ class CreateLikesTable extends Migration
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
 
-            $table->
+            $table->foreignId('user_id');
+            $table->foreignId('post_id');
 
             $table->timestamps();
         });
