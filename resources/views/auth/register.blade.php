af3608a777a5c99cb96b4a873a9b8560c9581e8b
RedGradient <>
2021-06-12 13:11:05 +1100

    Removed large logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,9 +46,9 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">--}}
+{{--                    {{ __('Already registered?') }}--}}
+{{--                </a>--}}
 
                 <x-button class="ml-4">
                     {{ __('Register') }}
