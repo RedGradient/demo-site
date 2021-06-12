fd7631a7d0caf9011b0a252c8cf1e57d944149d4
RedGradient <>
2021-06-12 16:56:26 +1100

    Like and Unlike functionality

--- public/js/likeIt.js
+++ public/js/likeIt.js
@@ -1,4 +1,10 @@
 function likeIt() {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
     // let like_label = document.getElementById('like_label');
     let like = document.getElementById('like');
     let like_count = document.getElementById('like_count');
@@ -6,9 +12,25 @@ function likeIt() {
     if (!like.checked) {
         let count = parseInt(like_count.innerText) + 1;
         like_count.innerText = " " + count.toString();
+
+        $.ajax({
+            type: "POST",
+            url: "/like",
+            data: {
+                "post_id": 1,
+            }
+        });
     } else {
         let count = parseInt(like_count.innerText) - 1;
         like_count.innerText = " " + count.toString();
+
+        $.ajax({
+            type: "POST",
+            url: "/unlike",
+            data: {
+                "post_id": 1,
+            }
+        });
     }
 
 
