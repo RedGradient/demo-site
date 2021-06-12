13197343b0f33e13f5649e57fcd623b75516e605
RedGradient <>
2021-06-12 17:33:19 +1100

    Removed

--- public/js/setFilterAndOrdering.js
+++
@@ -1,8 +0,0 @@
-let currentURL = window.location.href;
-let targetButton = Document.getElementById('sort');
-
-function addKey(e) {
-    let pressed = Document.getElementById(e.getId);
-    if (targetButton.getAttribute('href'));
-    // currentURL +=
-}
