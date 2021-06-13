1e70a8412f3471cf59b984beccb83e572d9c93f9
RedGradient <>
2021-06-13 13:08:22 +1100

    Created seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Rubric;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(5, true),
+            'description' => $this->faker->words(20, true),
+            'body' => $this->faker->paragraphs(6, true),
+            'image' => 'default.jpg',
+            'rubric_id' => Rubric::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Like;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Rubric;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        $this->create_rubrics();
+        Post::factory(20)->create();
+        Like::factory(50)->create();
+    }
+
+    public function create_rubrics() {
+        $rubrics = ['Бизнес', 'Политика', 'Экономика', 'Космос'];
+        foreach ($rubrics as $r) {
+            $rr = new Rubric;
+            $rr->title = $r;
+            $rr->save();
+        }
     }
 }
